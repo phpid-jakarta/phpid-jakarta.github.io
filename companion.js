e4c3e67e8b3fafe9ad96bb3ef993e12906062e1c
Irfan Maulana <mazipanneh@gmail.com>
2021-01-26 18:57:32 +0700

    Update companion.js

--- companion.js
+++ companion.js
@@ -1,10 +1,18 @@
 (function () {
   'use strict'
-  var workerScript = document.currentScript.dataset.serviceWorker
+//   var workerScript = document.currentScript.dataset.serviceWorker
   
-  if (workerScript && 'serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register(`${workerScript}`)
-    })
+//   if (workerScript && 'serviceWorker' in navigator) {
+//     window.addEventListener('load', function () {
+//       navigator.serviceWorker.register(`${workerScript}`)
+//     })
+//   }
+  
+  if ('serviceWorker' in window.navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      for (const registration of registrations) {
+        registration.unregister();
+      }
+    });
   }
 })()
